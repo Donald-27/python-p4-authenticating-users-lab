fbe6fc05ccf70ee1a5221688378ab4dc24c2836c
Donald Kiprop <kipropdonald27@gmail.com>
2025-06-20 17:12:49 +0300

    Add user authentication using Flask sessions with /login, /logout, /check_session routes and secure article view limits

--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s"
 })
 
 db = SQLAlchemy(metadata=metadata)
@@ -18,7 +18,6 @@ class Article(db.Model, SerializerMixin):
     preview = db.Column(db.String)
     minutes_to_read = db.Column(db.Integer)
     date = db.Column(db.DateTime, server_default=db.func.now())
-
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
@@ -33,4 +32,4 @@ class User(db.Model, SerializerMixin):
     articles = db.relationship('Article', backref='user')
 
     def __repr__(self):
-        return f'User {self.username}, ID {self.id}'
+        return f'U
